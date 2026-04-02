8cf83e5c46d99c58e9bd6232e38000e048395923
skid-dev <62094231+skid-dev@users.noreply.github.com>
2026-04-02 17:18:38 +1100

    build: Upgrade TypeScript version 4.5.4 -> 6.0.2 (#13)
    * refactor: Move launcher functions to launcher dir.
    
    * fix: capitalisation `SchoolTape` -> `Schooltape` (#11)
    
    * build: Bump module resolution version to Node16
    
    * build: Update tsconfig with updated moduleResolution
    
    * chore: Upgrade TypeScript version 4.5.4 -> 6.0.2
    
    * uhhh
    
    * chore: add types for chrome + enable strict mode
    
    * i hate typescript sm
    
    * build: Update Node version.
    
    ---------
    
    Co-authored-by: willow <42willow@pm.me>

--- src/storage/revisions/revision_get.ts
+++ src/storage/revisions/revision_get.ts
@@ -4,5 +4,5 @@ import { get_storage_key } from "./common"
 export async function get_revision(rev_id: string): Promise<RevisionHistoryEntry | null> {
     const item_name = get_storage_key(rev_id)
     const result = await chrome.storage.local.get([item_name])
-    return result[item_name] || null
+    return result[item_name] as RevisionHistoryEntry || null
 }
